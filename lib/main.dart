c4ba1dd9c9bc85ef3921670b98457924ca97dfba
Rahul  Pandey <rahul.pandey@cliffex.com>
2019-07-11 14:39:15 +0530

    Different way of handling onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,38 @@ import './answer.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
     // TODO: implement build
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
               title: Text('My First App'),
             ),
-            body: Text('Hello')));
+            body: Column(children: [
+              Text('The question!'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('Answer 2 chosen'),
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: (){
+                  print('Answer 3 chosen');
+                },
+              )
+            ])));
   }
 }
