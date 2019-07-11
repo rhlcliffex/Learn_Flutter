1ef586027f98dec2a810c4e260faf1086593f524
Rahul  Pandey <rahul.pandey@cliffex.com>
2019-07-11 13:53:00 +0530

    basic material print text

--- lib/main.dart
+++ lib/main.dart
@@ -4,66 +4,15 @@ import './quiz.dart';
 import './question.dart';
 import './answer.dart';
 
-/*void main() {
+void main() {
   runApp(MyApp());
-}*/
-
-void main() => MyApp();
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
 }
 
-class _MyAppState extends State {
-  var _questions = const [
-    {
-      'questionText': 'What\'s your favourite colour?',
-      'answers': ['saffron', 'blue', 'white', 'yellow', 'red']
-    },
-    {
-      'questionText': 'What\'s your favourite animal?',
-      'answers': ['dog', 'cat', 'cow', 'fox', 'lion']
-    },
-    {
-      'questionText': 'What\'s your favourite movie?',
-      'answers': ['housefull', 'Andaaz apna apna', 'Kesari', 'kmg']
-    }
-  ];
-
-  var _questionIndex = 0;
-
-  void _answerQuestion() {
-    setState(() {
-      _questionIndex++;
-      print(_questionIndex);
-    });
-  }
-
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First Flurrer App'),
-        ),
-        body: /*(_questionIndex < _questions.length)
-            ? */Column(
-                children: [
-                  Question(
-                      _questions[_questionIndex]['questionText']
-                  ),
-                  ...(_questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList()
-                ],
-              )
-            //: Center(child: Text('You did it!')),
-      ),
-    );
+    // TODO: implement build
+    return MaterialApp(home: Text('Hello'));
   }
+
 }
